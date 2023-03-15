93481aa049ba14c51724523c57c5ac98230be4e8
Karol <karolrds01@gmail.com>
2023-03-15 15:56:21 -0400

    header k

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <div className={styles.header}>
         <img src={icon} alt="Logo Nu kenzie" />
+        <span>With ❤️ by K</span>
     </div>
   )
 }
